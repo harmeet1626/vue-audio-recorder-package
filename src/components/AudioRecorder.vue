70c86218553370d372e21a4f81f5b309f816ffa3
Harmeet Singh <Harmeet_singh1@softprodigy.com>
2023-08-04 10:48:42 +0530

    error handling done when microphone not connected

--- src/components/AudioRecorder.vue
+++ src/components/AudioRecorder.vue
@@ -1,40 +1,31 @@
 <template>
     <div>
-        <button :style="{ backgroundColor: sBgColor, color: sTxtColor }"
-            class="start-recording-button" @click="startRecording" v-if="!isRecording">
+        <p id="error"></p>
+        <button :style="{ backgroundColor: sBgColor, color: sTxtColor }" class="start-recording-button"
+            @click="startRecording" v-if="!isRecording">
             <div style="display: flex;">
-                <span class="button-icon" :style="{'background-image': `url(${sBtnIcon})`}"></span>
+                <span class="button-icon" :style="{ 'background-image': `url(${sBtnIcon})` }"></span>
                 {{ sText }}
             </div>
         </button>
-        <button v-if="isRecording" :style="{ backgroundColor: spBgColor, color: spTxtColor }"
-            class="stop-recording-button" @click="stopRecording">
+        <button v-if="isRecording" :style="{ backgroundColor: spBgColor, color: spTxtColor }" class="stop-recording-button"
+            @click="stopRecording">
             <div style="display: flex;">
-                <span class="button-icon" :style="{'background-image': `url(${spBtnIcon})`}"></span>
+                <span class="button-icon" :style="{ 'background-image': `url(${spBtnIcon})` }"></span>
                 {{ spText }}
             </div>
         </button>
         <div v-if="isRecording">
             <div class="">
                 <div class="pulse">
-                    <Image  />
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        >
-                        <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
+                    <Image />
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M9 4C9 2.34315 10.3431 1 12 1C13.6569 1 15 2.34315 15 4V12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12V4ZM13 4V12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12V4C11 3.44772 11.4477 3 12 3C12.5523 3 13 3.44772 13 4Z"
-                            fill="currentColor"
-                        />
+                            fill="currentColor" />
                         <path
                             d="M18 12C18 14.973 15.8377 17.441 13 17.917V21H17V23H7V21H11V17.917C8.16229 17.441 6 14.973 6 12V9H8V12C8 14.2091 9.79086 16 12 16C14.2091 16 16 14.2091 16 12V9H18V12Z"
-                            fill="currentColor"
-                        />
+                            fill="currentColor" />
                     </svg>
                 </div>
             </div>
@@ -53,9 +44,9 @@ export default {
             streamVar: null,
             sText: this.settings.startButtonName || 'Start',
             spText: this.settings.stopButtonName || 'Stop',
-            sTxtColor: this.settings.startButtonTextColor || 'white' ,
+            sTxtColor: this.settings.startButtonTextColor || 'white',
             sBgColor: this.settings.startButtonBgColor || 'blue',
-            spTxtColor: this.settings.stopButtonTextColor || 'white' ,
+            spTxtColor: this.settings.stopButtonTextColor || 'white',
             spBgColor: this.settings.stopButtonBgColor || 'red',
             sBtnIcon: this.getImgUrl(this.settings.startButtonIcon, 'start'),
             spBtnIcon: this.getImgUrl(this.settings.stopButtonIcon, 'stop'),
@@ -65,12 +56,12 @@ export default {
     methods: {
         getImgUrl(pic, type) {
             console.log(pic)
-            if(pic) return pic;
+            if (pic) return pic;
 
-            if(type == 'start')
+            if (type == 'start')
                 return require('../assets/startButton.png')
-            else 
-            return require('../assets/stopButton.png')
+            else
+                return require('../assets/stopButton.png')
         },
         startRecording() {
             this.chunks = [];
@@ -92,9 +83,11 @@ export default {
                     };
                     this.mediaRecorder.start();
                     this.isRecording = true;
+                    document.getElementById("error").innerHTML = ''
                 })
                 .catch((error) => {
                     console.error("Error accessing the microphone:", error);
+                    document.getElementById("error").innerHTML = 'Error accessing the microphone'
                 });
         },
         stopRecording() {
@@ -117,15 +110,17 @@ export default {
     border-radius: 5px;
     font-size: 16px;
     cursor: pointer;
-    
+
 }
-.button-icon{
+
+.button-icon {
     margin-right: 0.5rem;
     background-size: contain;
     background-repeat: no-repeat;
     width: 16px;
     height: 16px;
 }
+
 .stop-recording-button {
     padding: 10px 20px;
     /* background-color: #db3434; */
@@ -147,19 +142,26 @@ export default {
     display: inline-block;
     margin: 15px;
 }
+
+#error {
+    color: red;
+}
+
 @keyframes animate {
     0% {
         box-shadow: 0 0 0 0 rgba(255, 0, 64, 0.7), 0 0 0 0 rgba(255, 0, 64, 0.7);
     }
+
     40% {
         box-shadow: 0 0 0 50px rgba(255, 0, 64, 0), 0 0 0 0 rgba(255, 0, 64, 0.7);
     }
+
     80% {
         box-shadow: 0 0 0 50px rgba(255, 0, 64, 0), 0 0 0 30px rgba(255, 0, 64, 0);
     }
+
     100% {
         box-shadow: 0 0 0 0 rgba(255, 0, 64, 0), 0 0 0 30px rgba(255, 0, 64, 0);
     }
 }
-
 </style>
